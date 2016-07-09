a1efef0e2b94f7a3e180f1f0e94e3513e5d3b13d
Klas <cassiuses@hotmail.com>
2016-07-09 21:32:15 +0200

    First draft of Shootout
    Added initial project

---
+++ Shootout/Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Controller : MonoBehaviour {
+
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float moveV = Input.GetAxis("Vertical");
+        float moveH = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveH, 0.0f, moveV);
+
+        Rigidbody body = GetComponent<Rigidbody>();
+
+        body.AddForce(movement * speed * Time.deltaTime);
+    }
+}
