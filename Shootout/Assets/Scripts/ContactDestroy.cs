3ae335bbd6a85fd9951ee290d7d8af31ad22ed82
Klas <cassiuses@hotmail.com>
2016-07-31 18:43:14 +0200

    Deleting shots on contact with boundary walls

---
+++ Shootout/Assets/Scripts/ContactDestroy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContactDestroy : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+
+        GameObject tmp = Instantiate(explosion, transform.position, transform.rotation) as GameObject;
+        Destroy(tmp, 1);
+
+        if (other.tag == "Player")
+        {
+            tmp = Instantiate(playerExplosion, other.transform.position, other.transform.rotation) as GameObject;
+            Destroy(tmp, 1);
+        }
+    }
+}
