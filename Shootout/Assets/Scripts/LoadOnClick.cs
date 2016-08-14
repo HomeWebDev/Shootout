c2b6bedf2162e4553ba87f8d5f4fd44a67fbdfb5
Klas <cassiuses@hotmail.com>
2016-08-14 21:13:45 +0200

    Added intro screen with choice of 2-4 players

---
+++ Shootout/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour
+{
+
+    // Use this for initialization
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
+
+    //public void LoadScene(int level)
+    //{
+    //    print("test");
+    //    Application.LoadLevel(level);
+
+    //}
+
+    public void LoadScene(string sceneName)
+    {
+        //print("test2");
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("scene1");
+    }
+
+    //public void LoadScene(int sceneBuildIndex, UnityEngine.SceneManagement.LoadSceneMode mode = UnityEngine.SceneManagement.LoadSceneMode.Single)
+    //{
+    //    print("test");
+
+    //    UnityEngine.SceneManagement.SceneManager.LoadScene(sceneBuildIndex);
+    //}
+
+    //void LoadScene()
+    //{
+    //    print("test");
+    //}
+
+}
